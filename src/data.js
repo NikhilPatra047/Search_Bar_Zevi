fe60048bc7b2e339662cf0afa1405cc749b35e88
Nikhil Kumar Patra <56184525+NikhilPatra047@users.noreply.github.com>
2022-06-17 15:10:15 +0530

    Created some dummy data

--- src/data.js
+++ src/data.js
@@ -1,2 +1,187 @@
 
 export const dataURL = "https://fakerapi.it/api/v1/books?_quantity=5";
+
+export const productURL = "https://fakerapi.it/api/v1/books?_quantity=12";
+
+export const product = [
+    {
+        id: 1,
+        title: "Let this be a.",
+        author: "Clara Ondricka",
+        image: "http://placeimg.com/480/640/any",
+        oldPrice: 599,
+        newPrice: 549,
+        checked: false
+    },
+    {
+        id: 2,
+        title: "Edwin and Morcar.",
+        author: "Jace Kunde",
+        image: "http://placeimg.com/480/640/any",
+        oldPrice: 699,
+        newPrice: 599,
+        checked: false
+    },
+    {
+        id: 3,
+        title: "But her sister sat.",
+        author: "Lauryn Kirlin",
+        image: "http://placeimg.com/480/640/any",
+        oldPrice: 799,
+        newPrice: 699,
+        checked: false
+    },
+    {
+        id: 4,
+        title: "Dormouse: 'not in.",
+        author: "Orville Schneider",
+        image: "http://placeimg.com/480/640/any",
+        oldPrice: 899,
+        newPrice: 800,
+        checked: false
+    },
+    {
+        id: 5,
+        title: "I can remember.",
+        author: "Angela Larson",    
+        image: "http://placeimg.com/480/640/any",
+        oldPrice: 799,
+        newPrice: 700,
+        checked: false
+    },
+    {
+        id: 6,
+        title: "Dormouse, without.",
+        author: "Elmira Bauch",
+        image: "http://placeimg.com/480/640/any",
+        oldPrice: 699,
+        newPrice: 650,
+        checked: false
+    },
+    {
+        id: 7,
+        title: "Alice replied.",
+        author: "Sarai Berge",
+        image: "http://placeimg.com/480/640/any",
+        oldPrice: 499,
+        newPrice: 399,
+        checked: false
+    },
+    {
+        id: 8,
+        title: "The judge, by the.",
+        author: "Vito Frami",
+        image: "http://placeimg.com/480/640/any",
+        oldPrice: 299,
+        newPrice: 350,
+        checked: false
+    },
+    {
+        id: 9,
+        title: "Queen, tossing her.",
+        author: "Ruthie Turner",
+        image: "http://placeimg.com/480/640/any",
+        oldPrice: 499,
+        newPrice: 399,
+        checked: false
+    },
+    {
+        id: 10,
+        title: "Alice; not that.",
+        author: "Chanelle Franecki",
+        image: "http://placeimg.com/480/640/any",
+        oldPrice: 599,
+        newPrice: 550,
+        checked: false
+    },
+    {
+        id: 11,
+        title: "The first witness.",
+        author: "Amya Howe",
+        image: "http://placeimg.com/480/640/any",
+        oldPrice: 500, 
+        newPrice: 450,
+        checked: false
+    },
+    {
+        id: 12,
+        title: "Alice, as she.",
+        author: "Novella Berge",
+        image: "http://placeimg.com/480/640/any",
+        oldPrice: 400,
+        newPrice: 350,
+        checked: false
+    },
+    {
+        id: 13,
+        title: "And it'll fetch.",
+        author: "Arvilla Hoppe",
+        image: "http://placeimg.com/480/640/any",
+        oldPrice: 600,
+        newPrice: 550,
+        checked: false
+    }, 
+    {
+        id: 14,
+        title: "Alice panted as.",
+        author: "Rex Gleason",
+        image: "http://placeimg.com/480/640/any",
+        oldPrice: 700,
+        newPrice: 650,
+        checked: false
+    },
+    {
+        id: 15,
+        title: "CHORUS. 'Wow! wow!.",
+        author: "Lauryn Becker",
+        image: "http://placeimg.com/480/640/any",
+        oldPrice: 650,
+        newPrice: 600,
+        checked: false
+    },
+    {
+        id: 16,
+        title: "WOULD always get.",
+        author: "Reuben Osinski",
+        image: "http://placeimg.com/480/640/any",
+        oldPrice: 850,
+        newPrice: 800,
+        checked: false
+    },
+    {
+        id: 17,
+        title: "I declare it's too.",
+        author: "Ernest Koelpin",
+        image: "http://placeimg.com/480/640/any",
+        oldPrice: 999,
+        newPrice: 899,
+        checked: false
+    },
+    {
+        id: 18,
+        title: "Alice after it.",
+        author: "Lora Grant",
+        image: "http://placeimg.com/480/640/any",
+        oldPrice: 799,
+        newPrice: 750,
+        checked: false
+    },
+    {
+        id: 19,
+        title: "I sleep\" is the.",
+        author: "Lauryn Schinner",
+        image: "http://placeimg.com/480/640/any",
+        oldPrice: 699,
+        newPrice: 650,
+        checked: false
+    }, 
+    {
+        id: 20,
+        title: "Alice: 'I don't.",
+        author: "Jordan Sipes",
+        image: "http://placeimg.com/480/640/any",
+        oldPrice: 599, 
+        newPrice: 499,
+        checked: false
+    }
+]
