6d498f53ee1c9f72b3265282f56aecd1c7622cad
Nikhil Kumar Patra <56184525+NikhilPatra047@users.noreply.github.com>
2022-06-17 12:37:54 +0530

    Created Routes to the pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import SearchBar from "./components/SearchBar";
+import MainPage from "./pages/MainPage";
+import ProductPage from "./pages/ProductPage";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <SearchBar />
-  );
-}
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<MainPage />} />
+                <Route path="/product" element={<ProductPage />} />
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
