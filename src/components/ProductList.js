c318150aec9c7cbef4f8299244bb12b69b721d21
Nikhil Kumar Patra <56184525+NikhilPatra047@users.noreply.github.com>
2022-06-17 07:30:28 +0530

    Created the ProductList component

---
+++ src/components/ProductList.js
@@ -0,0 +1,217 @@
+import { useGlobalContext } from "../context";
+import { productURL } from "../data";
+import { useFetch } from "../useFetch";
+
+function ProductList() {
+
+    const { data } = useFetch(productURL);
+    console.log(data);
+
+    const { check, toggleCheck } = useGlobalContext();
+
+    return (
+        <>
+            <h2 className="searchResultsTitle">Search Results</h2>
+            
+            <div className="product-container">
+
+                <div className="filterInfo">
+                    <div className="filter-item">
+                        <h4>BRAND</h4>
+                        <label class="container">Mango
+                            <input type="checkbox" />
+                            <span class="checkmark"></span>
+                        </label>
+                        <label class="container">H&M
+                            <input type="checkbox" />
+                            <span class="checkmark"></span>
+                        </label>
+                    </div>
+
+                    <div className="filter-item">
+                        <h4>PRICE RANGE</h4>
+                        <label class="container">Under 500
+                            <input type="checkbox" />
+                            <span class="checkmark"></span>
+                        </label>
+                        <label class="container">1000 To 3000
+                            <input type="checkbox" />
+                            <span class="checkmark"></span>
+                        </label>
+                    </div>
+
+                    <div className="filter-item">
+
+                        <h4>RATINGS</h4>
+                        <label class="container">
+                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 0L12.2451 6.90983H19.5106L13.6327 11.1803L15.8779 18.0902L10 13.8197L4.12215 18.0902L6.36729 11.1803L0.489435 6.90983H7.75486L10 0Z" fill="#FDD33D"/>
+                            </svg>
+                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 0L12.2451 6.90983H19.5106L13.6327 11.1803L15.8779 18.0902L10 13.8197L4.12215 18.0902L6.36729 11.1803L0.489435 6.90983H7.75486L10 0Z" fill="#FDD33D"/>
+                            </svg>
+                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 0L12.2451 6.90983H19.5106L13.6327 11.1803L15.8779 18.0902L10 13.8197L4.12215 18.0902L6.36729 11.1803L0.489435 6.90983H7.75486L10 0Z" fill="#FDD33D"/>
+                            </svg>
+                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 0L12.2451 6.90983H19.5106L13.6327 11.1803L15.8779 18.0902L10 13.8197L4.12215 18.0902L6.36729 11.1803L0.489435 6.90983H7.75486L10 0Z" fill="#FDD33D"/>
+                            </svg>
+                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 0L12.2451 6.90983H19.5106L13.6327 11.1803L15.8779 18.0902L10 13.8197L4.12215 18.0902L6.36729 11.1803L0.489435 6.90983H7.75486L10 0Z" fill="#FDD33D"/>
+                            </svg>
+
+                            <input type="checkbox" />
+                            <span class="checkmark"></span>
+                        </label>
+
+                        <label class="container">
+                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 0L12.2451 6.90983H19.5106L13.6327 11.1803L15.8779 18.0902L10 13.8197L4.12215 18.0902L6.36729 11.1803L0.489435 6.90983H7.75486L10 0Z" fill="#FDD33D"/>
+                            </svg>
+                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 0L12.2451 6.90983H19.5106L13.6327 11.1803L15.8779 18.0902L10 13.8197L4.12215 18.0902L6.36729 11.1803L0.489435 6.90983H7.75486L10 0Z" fill="#FDD33D"/>
+                            </svg>
+                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 0L12.2451 6.90983H19.5106L13.6327 11.1803L15.8779 18.0902L10 13.8197L4.12215 18.0902L6.36729 11.1803L0.489435 6.90983H7.75486L10 0Z" fill="#FDD33D"/>
+                            </svg>
+                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 0L12.2451 6.90983H19.5106L13.6327 11.1803L15.8779 18.0902L10 13.8197L4.12215 18.0902L6.36729 11.1803L0.489435 6.90983H7.75486L10 0Z" fill="#FDD33D"/>
+                            </svg>
+                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 0L12.2451 6.90983H19.5106L13.6327 11.1803L15.8779 18.0902L10 13.8197L4.12215 18.0902L6.36729 11.1803L0.489435 6.90983H7.75486L10 0Z" fill="#CDCCC8"/>
+                            </svg>
+
+                            <input type="checkbox" />
+                            <span class="checkmark"></span>
+                        </label>
+
+                        <label class="container">
+                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 0L12.2451 6.90983H19.5106L13.6327 11.1803L15.8779 18.0902L10 13.8197L4.12215 18.0902L6.36729 11.1803L0.489435 6.90983H7.75486L10 0Z" fill="#FDD33D"/>
+                            </svg>
+                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 0L12.2451 6.90983H19.5106L13.6327 11.1803L15.8779 18.0902L10 13.8197L4.12215 18.0902L6.36729 11.1803L0.489435 6.90983H7.75486L10 0Z" fill="#FDD33D"/>
+                            </svg>
+                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 0L12.2451 6.90983H19.5106L13.6327 11.1803L15.8779 18.0902L10 13.8197L4.12215 18.0902L6.36729 11.1803L0.489435 6.90983H7.75486L10 0Z" fill="#FDD33D"/>
+                            </svg>
+                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 0L12.2451 6.90983H19.5106L13.6327 11.1803L15.8779 18.0902L10 13.8197L4.12215 18.0902L6.36729 11.1803L0.489435 6.90983H7.75486L10 0Z" fill="#CDCCC8"/>
+                            </svg>
+                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 0L12.2451 6.90983H19.5106L13.6327 11.1803L15.8779 18.0902L10 13.8197L4.12215 18.0902L6.36729 11.1803L0.489435 6.90983H7.75486L10 0Z" fill="#CDCCC8"/>
+                            </svg>
+
+                            <input type="checkbox" />
+                            <span class="checkmark"></span>
+                        </label>
+
+                        <label class="container">
+                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 0L12.2451 6.90983H19.5106L13.6327 11.1803L15.8779 18.0902L10 13.8197L4.12215 18.0902L6.36729 11.1803L0.489435 6.90983H7.75486L10 0Z" fill="#FDD33D"/>
+                            </svg>
+                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 0L12.2451 6.90983H19.5106L13.6327 11.1803L15.8779 18.0902L10 13.8197L4.12215 18.0902L6.36729 11.1803L0.489435 6.90983H7.75486L10 0Z" fill="#FDD33D"/>
+                            </svg>
+                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 0L12.2451 6.90983H19.5106L13.6327 11.1803L15.8779 18.0902L10 13.8197L4.12215 18.0902L6.36729 11.1803L0.489435 6.90983H7.75486L10 0Z" fill="#CDCCC8"/>
+                            </svg>
+                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 0L12.2451 6.90983H19.5106L13.6327 11.1803L15.8779 18.0902L10 13.8197L4.12215 18.0902L6.36729 11.1803L0.489435 6.90983H7.75486L10 0Z" fill="#CDCCC8"/>
+                            </svg>
+                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 0L12.2451 6.90983H19.5106L13.6327 11.1803L15.8779 18.0902L10 13.8197L4.12215 18.0902L6.36729 11.1803L0.489435 6.90983H7.75486L10 0Z" fill="#CDCCC8"/>
+                            </svg>
+                            
+                            <input type="checkbox" />
+                            <span class="checkmark"></span>
+                        </label>
+
+                        <label class="container">
+                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 0L12.2451 6.90983H19.5106L13.6327 11.1803L15.8779 18.0902L10 13.8197L4.12215 18.0902L6.36729 11.1803L0.489435 6.90983H7.75486L10 0Z" fill="#FDD33D"/>
+                            </svg>
+                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 0L12.2451 6.90983H19.5106L13.6327 11.1803L15.8779 18.0902L10 13.8197L4.12215 18.0902L6.36729 11.1803L0.489435 6.90983H7.75486L10 0Z" fill="#CDCCC8"/>
+                            </svg>
+                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 0L12.2451 6.90983H19.5106L13.6327 11.1803L15.8779 18.0902L10 13.8197L4.12215 18.0902L6.36729 11.1803L0.489435 6.90983H7.75486L10 0Z" fill="#CDCCC8"/>
+                            </svg>
+                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 0L12.2451 6.90983H19.5106L13.6327 11.1803L15.8779 18.0902L10 13.8197L4.12215 18.0902L6.36729 11.1803L0.489435 6.90983H7.75486L10 0Z" fill="#CDCCC8"/>
+                            </svg>
+                            <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 0L12.2451 6.90983H19.5106L13.6327 11.1803L15.8779 18.0902L10 13.8197L4.12215 18.0902L6.36729 11.1803L0.489435 6.90983H7.75486L10 0Z" fill="#CDCCC8"/>
+                            </svg>
+                            
+                            <input type="checkbox" />
+                            <span class="checkmark"></span>
+                        </label>
+
+                    </div>
+
+                </div>
+
+                <div className="productDisplay">
+                    <div className="bookContainer bookDisplay">
+                    { data.map((book) => {
+                        const { id, title, author, image } = book;
+                        return (
+                            <div className="book">
+                                <img src={image} />
+
+                                <svg className={ check ? "checked" : "unchecked"} 
+                                onClick={() => toggleCheck(id, data)}
+                                width="27" height="24" viewBox="0 0 27 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M8.09375 1.01172C3.86581 1.01172 1.02734 4.43396 1.02734 8.66016C1.02734 13.4558 7.72721 19.1855 13.4688 23.0117C19.2103 19.1855 25.8327 13.3634 25.9102 8.66016C25.9798 4.43456 23.2436 1.01172 19.0156 1.01172C17.2119 1.01172 15.3826 1.96591 13.4688 3.87891C11.5549 1.96591 9.89749 1.01172 8.09375 1.01172Z" stroke="white"/>
+                                </svg>
+
+                                <p>{title}</p>
+
+                                <p style={{
+                                    marginTop: "0",
+                                    display: "inline-block",
+                                    width: "71px",
+                                    height: "24px",
+                                    right: "168px",
+                                    top: "385px",
+                                    fontFamily: 'Inter',
+                                    fontStyle: "normal",
+                                    fontWeight: "300",
+                                    fontSize: "20px",
+                                    lineHeight: "24px",
+                                    textDecorationLine: "line-through",
+                                    color: "rgba(0, 0, 0, 0.4)"}}
+                                >
+                                    Rs.599
+                                </p>
+
+                                <p style={{
+                                    marginTop: "0",
+                                    display: "inline-block",
+                                    width: "69px",
+                                    height: "24px",
+                                    right: "94px",
+                                    top: "385px",
+                                    fontFamily: 'Inter',
+                                    fontStyle: "normal",
+                                    fontWeight: "600",
+                                    fontSize: "20px",
+                                    lineHeight: "24px",
+                                    color: "#6D84FF"}}
+                                >
+                                    Rs.599
+                                </p>
+
+                            </div>
+                        );
+                    })}
+                    </div>
+                </div>
+
+            </div>
+        </>
+    );
+};
+
+export default ProductList;
