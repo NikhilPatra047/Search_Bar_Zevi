b0dbdf8afe42356df9bba6dd7db5347a6a3d1e24
Nikhil Kumar Patra <56184525+NikhilPatra047@users.noreply.github.com>
2022-06-17 12:38:28 +0530

    Created the Main Page

---
+++ src/pages/MainPage.js
@@ -0,0 +1,22 @@
+import ProductList from "../components/ProductList";
+import SearchBar from "../components/SearchBar";
+import TrendingBox from "../components/TrendingBox";
+import { useGlobalContext } from "../context";
+
+function MainPage() {
+  const { active } = useGlobalContext();
+
+  return (
+    <>
+      <div className="main">
+        <div className="company-logo"></div>  
+        <div className="search">
+          <SearchBar />
+          {active ? <TrendingBox /> : ``}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default MainPage;
